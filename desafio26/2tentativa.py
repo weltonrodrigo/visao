9bcb016397108d07de7a262e7980b1fe8e38b388
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2020-08-26 17:25:32 -0300

    Versão pronta para entregar

--- desafio26/2tentativa.py
+++ desafio26/2tentativa.py
@@ -13,7 +13,6 @@ from logging import warning
 bg = cv2.imread('bg.jpg', cv2.IMREAD_COLOR)
 bg = cv2.resize(bg, dsize=(1280, 720), interpolation=cv2.INTER_CUBIC)
 
-viewer = pyglview.Viewer()
 cap = acapture.open(0) # Camera 0,  /dev/video0
 
 
@@ -21,8 +20,41 @@ got_sample = False
 sample_range = None
 sample_size = (10, 10)
 threshold = 20
+step = 1
 
 kernel = np.ones((10,10), np.uint8)
+
+def change(k):
+    global sample_range
+
+    low = sample_range[0]
+    high = sample_range[1]
+    
+    print('k is', k)
+
+    if k == '1':
+        low[0] = low[0] - step
+        high[0] = high[0] + step
+    if k == '2':
+        low[1] = low[1] - step
+        high[1] = high[1] + step
+    if k == '3':
+        low[2] = low[2] - step
+        high[2] = high[2] + step
+
+    if k == 'q':
+        low[0] = low[0] + step
+        high[0] = high[0] - step
+    if k == 'w':
+        low[1] = low[1] + step
+        high[1] = high[1] - step
+    if k == 'e':
+        low[2] = low[2] + step
+        high[2] = high[2] - step
+
+    sample_range = (restrict(low), restrict(high))
+    
+    print('new range is', sample_range)
 
 def restrict(color):
     # https://docs.opencv.org/trunk/df/d9d/tutorial_py_colorspaces.html
@@ -46,8 +78,10 @@ def color_range4(sample):
     print('Low is: ', low)
     print('High is: ', high)
 
-    low = restrict(hsv.mean(axis=(0,1)) - [20, 100, 100])
-    high = restrict(high + [4, 100, 100])
+    amount = [10, 100, 100]
+
+    low = restrict(hsv.mean(axis=(0,1)) - amount)
+    high = restrict(hsv.mean(axis=(0,1)) + amount)
 
     print('Low is: ', low)
     print('High is: ', high)
@@ -114,6 +148,31 @@ def color_range(sample):
 
     got_sample = True
 
+def keyboard_listener(key, x, y):
+    print(key)
+    print(chr(key))
+    global got_sample
+    if key == ord('c'):
+        exit(0)
+    if key == ord('r'):
+        got_sample=False
+    if chr(key) in '123qwe':
+        change(chr(key))
+
+def text(image):
+    if got_sample:
+        low=  'Low:  hsv({:3.0f}, {:3.0f}, {:3.0f})'.format(*sample_range[0])
+        high= 'High: hsv({:3.0f}, {:3.0f}, {:3.0f})'.format(*sample_range[1])
+    else:
+        low = high = 'None'
+
+    image = cv2.putText(image, low, (20, 20), cv2.FONT_HERSHEY_SIMPLEX,
+            1, (255, 0, 0), 2, cv2.LINE_AA)
+
+    image = cv2.putText(image, high, (20, 65), cv2.FONT_HERSHEY_SIMPLEX,
+            1, (255, 0, 0), 2, cv2.LINE_AA)
+    return image
+
 def loop():
     check,frame = cap.read() # non-blocking
 
@@ -133,12 +192,14 @@ def loop():
 
             frame = frame + bg_masked
 
+        frame = text(frame)
         viewer.set_image(frame)
 
 def sample_cb(c):
     global got_sample
-    color_range2(c)
+    color_range4(c)
 
+viewer = pyglview.Viewer(keyboard_listener=keyboard_listener)
 viewer.set_sample_size(sample_size)
 viewer.set_sample_cb(sample_cb)
 viewer.set_loop(loop)
--- desafio26/4tentativa.py
+++ desafio26/4tentativa.py
@@ -2,29 +2,59 @@ import cv2
 import acapture
 import sys
 import numpy as np
-import colorsys
 
 sys.path.append('./python_glview') 
-
 import pyglview
-from OpenGL.GLUT import *
-from logging import warning
 
 bg = cv2.imread('bg.jpg', cv2.IMREAD_COLOR)
 bg = cv2.resize(bg, dsize=(1280, 720), interpolation=cv2.INTER_CUBIC)
 
-viewer = pyglview.Viewer()
 cap = acapture.open(0) # Camera 0,  /dev/video0
-
 
 got_sample = False
 sample_range = None
 sample_size = (10, 10)
 threshold = 20
+step = 1
 
 kernel = np.ones((10,10), np.uint8)
 
+def change(k):
+    '''Increase or decrease the range or masked colors'''
+    global sample_range
+
+    if not got_sample:
+        return None
+
+    low = sample_range[0]
+    high = sample_range[1]
+    
+    if k == '1':
+        low[0] = low[0] - step
+        high[0] = high[0] + step
+    if k == '2':
+        low[1] = low[1] - step
+        high[1] = high[1] + step
+    if k == '3':
+        low[2] = low[2] - step
+        high[2] = high[2] + step
+
+    if k == 'q':
+        low[0] = low[0] + step
+        high[0] = high[0] - step
+    if k == 'w':
+        low[1] = low[1] + step
+        high[1] = high[1] - step
+    if k == 'e':
+        low[2] = low[2] + step
+        high[2] = high[2] - step
+
+    sample_range = (restrict(low), restrict(high))
+    
+    print('new range is', sample_range)
+
 def restrict(color):
+    '''Clip color value to a valid HSV value in OpenCV'''
     # https://docs.opencv.org/trunk/df/d9d/tutorial_py_colorspaces.html
     # Max and min values of H, S, V in opencv
     min = (0, 0, 0)
@@ -32,7 +62,29 @@ def restrict(color):
 
     return np.clip(color, min, max)
 
+def color_range5(sample):
+    '''Approach #5: based on the mean of a sample, but ignoring value component'''
+    global sample_range, got_sample
+
+    # turn the bytearray into an (x, x, 3) np array (sample size with 3 colors)
+    img = np.frombuffer(sample, dtype=np.uint8).reshape(*sample_size, 3)
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    print('HSV is:', hsv)
+
+    # Range. 500 in V component will clip to 0 and 255
+    amount = [10, 100, 500]
+
+    low = restrict(hsv.mean(axis=(0,1)) - amount)
+    high = restrict(hsv.mean(axis=(0,1)) + amount)
+
+    sample_range = (low, high)
+
+    got_sample = True
+
+
 def color_range4(sample):
+    '''Approach #4: band pass filter based on mean'''
     global sample_range, got_sample
 
     # turn the bytearray into an (x, x, 3) np array (sample size with 3 colors)
@@ -46,8 +98,10 @@ def color_range4(sample):
     print('Low is: ', low)
     print('High is: ', high)
 
-    low = restrict(hsv.mean(axis=(0,1)) - [20, 100, 100])
-    high = restrict(high + [4, 100, 100])
+    amount = [10, 100, 100]
+
+    low = restrict(hsv.mean(axis=(0,1)) - amount)
+    high = restrict(hsv.mean(axis=(0,1)) + amount)
 
     print('Low is: ', low)
     print('High is: ', high)
@@ -55,7 +109,10 @@ def color_range4(sample):
     sample_range = (low, high)
 
     got_sample = True
+
+
 def color_range3(sample):
+    '''Approach #3: extend the min and the max values with a fixed amount'''
     global sample_range, got_sample
 
     # turn the bytearray into an (x, x, 3) np array (sample size with 3 colors)
@@ -78,7 +135,11 @@ def color_range3(sample):
     sample_range = (low, high)
 
     got_sample = True
+
+
 def color_range2(sample):
+    '''Approach #2: analise the sample to find distribution of values
+    and choose a window based on it'''
     global sample_range, got_sample
 
     # turn the bytearray into an (x, x, 3) np array (sample size with 3 colors)
@@ -102,6 +163,7 @@ def color_range2(sample):
     got_sample = True
 
 def color_range(sample):
+    '''Approach #1: get min and max values from the sample, use it as the range'''
     global sample_range, got_sample
 
     # turn the bytearray into an (x, x, 3) np array (sample size with 3 colors)
@@ -113,6 +175,38 @@ def color_range(sample):
     sample_range = (hsv.min(axis=(0,1)), hsv.max(axis=(0,1)))
 
     got_sample = True
+
+def keyboard_listener(key, x, y):
+    global got_sample
+
+    print(chr(key))
+    if key == ord('c'):
+        exit(0)
+    if key == ord('r'):
+        got_sample=False
+    if chr(key) in '123qwe':
+        change(chr(key))
+
+def sample_cb(c):
+    '''Callback to get sample pixels from opengl window
+    it come as a bytearray'''
+    global got_sample
+    color_range5(c)
+
+def text(image):
+    '''Add description text to the frame'''
+    if got_sample:
+        low=  'Low:  hsv({:3.0f}, {:3.0f}, {:3.0f})'.format(*sample_range[0])
+        high= 'High: hsv({:3.0f}, {:3.0f}, {:3.0f})'.format(*sample_range[1])
+    else:
+        low = high = 'None'
+
+    image = cv2.putText(image, low, (20, 20), cv2.FONT_HERSHEY_SIMPLEX,
+            1, (255, 0, 0), 2, cv2.LINE_AA)
+
+    image = cv2.putText(image, high, (20, 65), cv2.FONT_HERSHEY_SIMPLEX,
+            1, (255, 0, 0), 2, cv2.LINE_AA)
+    return image
 
 def loop():
     check,frame = cap.read() # non-blocking
@@ -133,12 +227,13 @@ def loop():
 
             frame = frame + bg_masked
 
+        # Add text
+        frame = text(frame)
+
+        # Set the frame to OpenGl window
         viewer.set_image(frame)
 
-def sample_cb(c):
-    global got_sample
-    color_range2(c)
-
+viewer = pyglview.Viewer(keyboard_listener=keyboard_listener)
 viewer.set_sample_size(sample_size)
 viewer.set_sample_cb(sample_cb)
 viewer.set_loop(loop)
