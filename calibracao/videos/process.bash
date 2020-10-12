e69f70a28d627e9630942fb8b48a948af8fb582d
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2020-10-12 20:59:06 -0300

    Perfeitamente sincronizados.
    Não entendi porque funcionou, mas alterei o vsync para 1 na geração
    dos vídeos com framerate 10 e vsync 0 na geração dos timecodes.
    
    Vídeo é o calibracao/videos/versao-perfeitamente-sincronizada.mp4

--- calibracao/videos/process.bash
+++ calibracao/videos/process.bash
@@ -1,9 +1,9 @@
 # vim:ts=4 sw=4 nowrap:
 # Tempo que funcionou em 25fps: 4.45
-ffmpeg -y -i ../trabalho1/camera1.webm.bkp -vf scale=1280:720 -ss 4.45 \
-	-r 10 -vsync 2 -crf 28 -q 1 -c:v h264_videotoolbox -q:v 1 -b:v 10000k timestamp-cam-1.mp4
+ffmpeg -y -i ../trabalho1/camera1.webm.bkp -vf scale=1280:720 -ss 4.51449102005957 \
+	-r 10 -vsync 1 -crf 28 -q 1 -c:v h264_videotoolbox -q:v 1 -b:v 10000k timestamp-cam-1.mp4
 ffmpeg -y -i ../trabalho1/camera2.webm                       					   \
-	-r 10 -vsync 2 -crf 28 -q 1 -c:v h264_videotoolbox -q:v 1 -b:v 10000k timestamp-cam-2.mp4
+	-r 10 -vsync 1 -crf 28 -q 1 -c:v h264_videotoolbox -q:v 1 -b:v 10000k timestamp-cam-2.mp4
 
 ffmpeg \
 	-i timestamp-cam-1.mp4\
@@ -13,6 +13,7 @@ ffmpeg \
 		  [1]drawtext=text=frame %{n}tempo %{pts}:fontsize=72:x=(w-tw)/2: y=h-(2*lh):fontcolor=white:box=1:boxcolor=0x00000099[1t];\
 		  [1:a][0:a]join=inputs=2:channel_layout=stereo, asplit[stereo][stereo1];\
 		  [1t][0t]hstack=shortest=1[v]" \
+	-vsync 0 \
 	-pix_fmt yuv420p -crf 1  -preset veryslow \
 	-map '[stereo]' -map '[v]' \
 	-c:v h264_videotoolbox -q:v 1 -b:v 1000k \
